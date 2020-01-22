65db1010ee0f1a620c86c1fdbd94bd9033d875eb
Kaiwen Wei <kevinawei@gmail.com>
2020-01-22 08:37:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // add solution here
 function theBeatlesPlay(x, y){
   var beatles = [];
-  for ( i = 0; i<x.length(); i++){
+  for (i = 0; i<x.length(); i++){
     var a = x[i] + " plays "+ y[i];
     beatles.add(a);
   }
